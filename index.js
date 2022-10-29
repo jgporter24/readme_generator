cf6b1ce7745d62b4eb8691ba8ed62a8963fe7c6f
jgporter24 <110738676+jgporter24@users.noreply.github.com>
2022-10-29 13:19:43 -0700

    updated utility folder, fixed some bugs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const util = require('util');
 const emailValidator = require('email-validator');
 
-const generateMarkdown = require('./utility/markdowngenerate.js');
+const generateMarkdown = require('./utility/markdowngenerate');
 
 //Array of quesitons
 const questions = [
